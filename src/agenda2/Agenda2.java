11c9c49453f37655c70157b75a13dcc83e5f47f5
Hugo Segura <shugo.alva20@gmail.com>
2023-10-15 21:01:25 -0500

    Subiendo primera parte de ejercicio programados los botones de atras y adelante

---
+++ src/agenda2/Agenda2.java
@@ -0,0 +1,15 @@
+
+package agenda2;
+
+import agenda2.vista.PantallaPrincipal;
+
+public class Agenda2 {
+
+    public static void main(String[] args) {
+        PantallaPrincipal pantalla1 = new PantallaPrincipal();
+        pantalla1.setVisible(true);
+        pantalla1.setLocale(null);        
+        
+    }
+    
+}
